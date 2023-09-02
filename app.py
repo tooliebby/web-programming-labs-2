f9769ee2521d9480e9a51bdc3e4f7825aadbe0f3
Георгий Гритчин <ggritcin1@gmail.com>
2023-09-02 13:07:45 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Гритчин Георгий, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная 1
+        </header>        
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Гритчин Георгий, ФБИ-12, 3 курс, 2023
+        </footer>
+    <body>
+</html> 
+"""
+
+
