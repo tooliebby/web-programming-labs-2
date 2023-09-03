978c633124b45acd474a94ebdbc042446d94a695
Георгий Гритчин <ggritcin1@gmail.com>
2023-09-03 15:39:45 +0700

    Меню

--- app.py
+++ app.py
@@ -2,8 +2,40 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
     return """
+<!doctype html>
+<html>
+    <head>
+        <title>Гритчин Георгий, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>        
+
+        <h1>web-сервер на flask</h1>
+
+        <h2>Меню лабораторных работ</h1>
+
+        <main>
+            <ol>
+                <li>
+                    <a href="http://127.0.0.1:5000/lab1" target="_blank">Лабораторная работа №1</a>
+                </li>
+            </ol>
+        </main>
+
+        <footer>
+            &copy; Гритчин Георгий, ФБИ-12, 3 курс, 2023
+        </footer>
+    <body>
+</html> 
+"""
+@app.route("/lab1")
+def lab1():
+    return"""
 <!doctype html>
 <html>
     <head>
@@ -15,6 +47,14 @@ def start():
         </header>        
 
         <h1>web-сервер на flask</h1>
+
+            <h5>
+            Flask — фреймворк для создания веб-приложений на языке
+            программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+            называемых микрофреймворков — минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+            </h5>
 
         <footer>
             &copy; Гритчин Георгий, ФБИ-12, 3 курс, 2023
