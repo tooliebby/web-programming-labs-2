4de4ccf303a912691a7d6605a3d7a292f795073e
Георгий Гритчин <ggritcin1@gmail.com>
2023-09-16 19:20:51 +0700

    Книги

--- app.py
+++ app.py
@@ -182,4 +182,16 @@ def example():
         {'name':'мандарины','price':'259₽'},
         {'name':'манго','price':'199₽'}
         ]
-    return render_template('example.html', name=name, groupe=groupe,course=course, fruits=fruits)
+    books = [
+        {'name': '1984', 'name_author': 'Джордж Оруэлл', 'zanr': 'Антиутопия', 'kol_stranits': '328'},
+        {'name': 'Преступление и наказание', 'name_author': 'Федор Достоевский', 'zanr': 'Роман', 'kol_stranits': '592'},
+        {'name': 'Гарри Поттер и философский камень', 'name_author': 'Дж. К. Роулинг', 'zanr': 'Фэнтези', 'kol_stranits': '320'},
+        {'name': 'Война и мир', 'name_author': 'Лев Толстой', 'zanr': 'Роман', 'kol_stranits': '1225'},
+        {'name': 'Убийство в Восточном экспрессе', 'name_author': 'Агата Кристи', 'zanr': 'Детектив', 'kol_stranits': '256'},
+        {'name': 'Алиса в Стране чудес', 'name_author': 'Льюис Кэрролл', 'zanr': 'Приключения', 'kol_stranits': '240'},
+        {'name': 'Мастер и Маргарита', 'name_author': 'Михаил Булгаков', 'zanr': 'Роман', 'kol_stranits': '448'},
+        {'name': 'Три товарища', 'name_author': 'Эрих Мария Ремарк', 'zanr': 'Роман', 'kol_stranits': '416'},
+        {'name': 'Самый богаты человек в вавилоне', 'name_author': 'Джордж Оруэлл', 'zanr': 'Антиутопия', 'kol_stranits': '156'},
+        {'name': 'Анна Каренина', 'name_author': 'Лев Толстой', 'zanr': 'Роман', 'kol_stranits': '864'}
+    ]
+    return render_template('example.html', name=name, groupe=groupe,course=course, fruits=fruits, books=books)
