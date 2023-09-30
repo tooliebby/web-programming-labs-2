22dc632d743252e262445c80b63b3106891ff934
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-09-30 11:01:20 +0700

    Добавлены ссылка в меню Лабораторной №2

--- app.py
+++ app.py
@@ -28,6 +28,9 @@ def menu():
                 <li>
                     <a href="http://127.0.0.1:5000/lab1" target="_blank">Лабораторная работа №1</a>
                 </li>
+                <li>
+                    <a href="http://127.0.0.1:5000/lab2" target="_blank">Лабораторная работа №2</a>
+                </li>
             </ol>
         </main>
 
